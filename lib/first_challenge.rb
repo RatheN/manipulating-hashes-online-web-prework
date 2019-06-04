dcb11f73dcae0a0ff918a72f3f390659f861f1d4
Nathan Rathe <nprathe10@gmail.com>
2019-06-04 21:32:55 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,8 +14,13 @@ def first_challenge
   }
 
   #your code here
-  favorite_icecream_flavors.delete_if do |flavor|
+  contacts. each do |person, data|
+    data.each do |attribute, value|
+      if attribute == :favorite_icecream_flavors
+      value.delete_if do |flavor|
     flavor == "strawberry"
+      end
+    end
   end
   #remember to return your newly altered contacts hash!
   contacts
